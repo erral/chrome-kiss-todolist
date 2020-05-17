ccce02ec74b31179bd2878e50666f86773846815
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-05-17 15:15:50 +0200

    do not cleanup todo-list on installing

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
 "use strict";
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ todos: Array() }, (e) => {
-    console.log("Todo list initialized");
-  });
+  console.log("Todo list initialized");
 });
